ce6270222a54bbb7f9a33e67f34bbbf772792eaf
swagatsahu-07 <swagatsahu9090@gmail.com>
2026-03-24 19:26:03 +0530

    cors routes changed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin:  [
+    "http://localhost:5173",
+    "https://interview-ai-pi-woad.vercel.app"
+  ],
   credentials: true
 }))
 
