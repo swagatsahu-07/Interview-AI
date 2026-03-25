9585d59757175a447fa3983241ea7c20d39b63c5
swagatsahu-07 <swagatsahu9090@gmail.com>
2026-03-25 12:43:24 +0530

    try catch added

--- Frontend/src/features/services/auth.api.js
+++ Frontend/src/features/services/auth.api.js
@@ -7,30 +7,30 @@ const api = axios.create({
 });
 
 export async function register({ userName, email, password }) {
-  try {
-    const response = await api.post("/api/auth/register", {
-      userName,
-      email,
-      password,
-    });
-
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+  const response = await api.post("/api/auth/register", {
+    userName,
+    email,
+    password,
+  });
+  return response.data;
 }
 
 export async function login({ email, password }) {
-  try {
-    const response = await api.post("/api/auth/login", {
-      email,
-      password,
-    });
+  const response = await api.post("/api/auth/login", {
+    email,
+    password,
+  });
+  return response.data;
+}
 
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+export async function logout() {
+  const response = await api.get("/api/auth/logout");
+  return response.data;
+}
+
+export async function getMe() {
+  const response = await api.get("/api/auth/get-me");
+  return response.data;
 }
 
 export async function logout() {
