1f7010ec7b20c0029ff866d884ad06c7c863a8dc
swagatsahu-07 <swagatsahu9090@gmail.com>
2026-03-25 11:49:36 +0530

    path added

--- Frontend/src/features/services/auth.api.js
+++ Frontend/src/features/services/auth.api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  // baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
